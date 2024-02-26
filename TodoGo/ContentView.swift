1cf4d7970cf5f6ed721b0f520ef54bd35c56e75b
seongmin kim <kimsklavier@gmail.com>
2024-02-26 17:18:44 +0900

    Initial Commit

---
+++ TodoGo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TodoGo
+//
+//  Created by 김성민 on 2/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
